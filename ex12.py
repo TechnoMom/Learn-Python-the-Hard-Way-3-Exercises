dc1c59752e9b29fd72893963451413d95086d6c2
Cyn Armistead <cyn@owloutcomes.com>
2017-02-28 14:11:46 -0500

    First 35 exercises

---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = raw_input("How old are you? ")
+height = raw_input("How many inches tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %s years old, %s inches tall and weigh %s pounds." % (
+    age, height, weight)
---
+++ ex3.py
@@ -0,0 +1,30 @@
+# prints the text
+print "I will now count my chickens:"
+
+# prints the text and 30
+print "Hens", 25 + 30 / 6
+# prints the text and 97
+print "Roosters", 100 - 25 * 3 % 4
+# prints the text
+print "Now I will count the eggs:"
+
+# prints 7
+print 3 + 2 + 1 - 5 + 4 % 2 - 1/4 + 6
+# prints text
+print "Is it true that 3 + 2 < 5 - 7?"
+# prints False
+print 3 + 2 < 5 - 7
+# prints text and 5
+print "What is 3 + 2?" , 3 + 2
+# prints text and -2
+print "What is 5 - 7?", 5 - 7
+# prints text
+print "Oh, that's why it's False."
+# prints text
+print "How about some more."
+# prints text and True
+print "Is it greater?", 5 > -2
+# printd text and True
+print "Is it greater or equal?", 5 >= -2
+# prints text and False
+print "Is it less or equal?", 5 <= -2
